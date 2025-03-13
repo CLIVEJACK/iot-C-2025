afde35fc3c52547b8d028e4d0fdffdd026189e00
DongKuk-Kim <yum0153@gmail.com>
2025-03-13 23:46:06 +0900

    3일차 아직남음

--- Day3/change.cpp
+++ Day3/change.cpp
@@ -1,25 +1,25 @@
-#define _CRT_SECURE_NO_WARNINGS
-# include <stdio.h>
-
-char change(char a, char b);
-
-int main()
-{
-	char ch1;
-	char ch2;
-	printf("문자를 입력하세요: ");
-	scanf("%c", &change);
-
-	printf("대문자: %c, 소문자: %c", ch1, ch2);
-
-	char val()
-
-	return 0;
-}
-
-char change(char a, char b) {
-	if ((a >= 'A') && (b <= 'Z')) {
-		a = b + ('a' - 'A');
-	}
-	return a, b;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//# include <stdio.h>
+//
+//char change(char a, char b);
+//
+//int main()
+//{
+//	char ch1;
+//	char ch2;
+//	printf("문자를 입력하세요: ");
+//	scanf("%c", &change);
+//
+//	printf("대문자: %c, 소문자: %c", &ch1, &ch2);
+//
+//	//char val()
+//
+//	return 0;
+//}
+//
+//char change(char a, char b) {
+//	if ((a >= 'A') && (b <= 'Z')) {
+//		a = b + ('a' - 'A');
+//	}
+//	return a, b;
+//}
---
+++ Day3/test.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+# include <stdio.h>
+
+char change_up(char a);
+char change_do(char a);
+int main()
+{
+	char c = 0;
+
+	printf("문자를 입력하세요:");
+	scanf("%c", &c);
+
+	printf("대문자는 : %c, 소문자는 %c 입니다\n", &change_up, &change_do);
+
+	return 0;
+
+}
+char change_up(char a) {
+	if (a > 96 && a < 123) a = a - 32;
+	return a;
+}
+char change_do(char a) {
+	if (a > 64 && a < 91) a = a + 32;
+	return a;
+}
+

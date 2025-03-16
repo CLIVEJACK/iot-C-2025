3bade38278d832576fe4ef68eca98dbb6d55f3ae
DongKuk-Kim <yum0153@gmail.com>
2025-03-16 12:39:39 +0900

    4일차 2차원배열 커밋

---
+++ Day4/test.c
@@ -0,0 +1,39 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int main()
+//{
+//
+//	double agv;
+//	int ary[4][3];
+//	int sum[4];
+//
+//	for (int i = 0; i < 4; i++) {
+//		printf("%d번째 학생 : ", i + 1);
+//		for (int j = 0; j < 3; j++) {
+//			scanf("%d번재 학생", &ary[i][j]);
+//		}
+//	}
+//	for (int i = 0; i < 4; i++) {
+//		sum[i] = 0;
+//		for (int j = 0; j < 3; j++) {
+//			sum[i] += ary[i][j];
+//		}
+//	}
+//	for (int i = 0; i < 4; i++) {
+//		printf("%d번학생: ", i + 1);
+//		for (int j = 0; j < 3; j++) {
+//			printf("%d", ary[i][j]);
+//		}
+//		agv = sum[i] / 3.0;
+//		printf(" %d %.2lf\n", sum[i], agv);
+//
+//	}
+//	
+//	
+//	
+//	return 0;
+//	
+//
+//
+//}
